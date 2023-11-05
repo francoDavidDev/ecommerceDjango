2bcf35b680ce22064f2d06b9bd0df516c3936502
franco66 <david_franco@outlook.com>
2023-11-05 18:51:51 -0300

    s

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-l_ygddvl#3+$7!^@2h%)yg#av36004g+=-)-k&%o#0h(r^8_ij'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in productions!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
